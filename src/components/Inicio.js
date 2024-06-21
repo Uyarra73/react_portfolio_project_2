c562e74073a50ad6966fdb188c855159b0d79f59
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-06-21 10:37:12 +0900

    Back

---
+++ src/components/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicio = () => {
+  return (
+    <div>
+      <h1>Hola! Soy Alberto Rosell y soy desarrollador Web. Ofrezco servicios de programcion y desrrollo en todo tipo de proyectos.</h1>
+      <h2>Si quieres contactarme, puedes hacerlo a traves de este formulario:</h2>
+    </div>
+  )
+}
+
+export default Inicio
