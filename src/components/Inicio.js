b6033450eeaba32d806905d56209f8d9dcf28189
Alberto Rosell Uyarra <roselluyarra@gmail.com>
2024-06-21 12:23:02 +0900

    Services page

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Inicio = () => {
   return (
     <div>
-      Inicio
+      <h1>Hola! Soy Alberto Rosell y soy desarrollador Web</h1>
+      <br/>
+      <h2>Te ayudo a desarrolladar to proyecto web desde cero. <Link to='/contacto'>Contacta conmigo!</Link></h2>
+      <br/>
+      <section className='last-works'>
+        <h2>Algunos de mis proyectos</h2>
+        <p>Echale un vistazo a algunos de mis trabajos:</p>
+        <div className='works'></div>
+      </section>
     </div>
   )
 }
--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Servicios = () => {
   return (
-    <div>
-      Servicios
+    <div className='page'>
+      <h1>Servicios</h1>
+
+      <section className='services'>
+        <article className='service'>
+          <h2>Diseño web desde cero</h2>
+          <p>Hacemos realidad tus ideas.</p>
+        </article>
+        <article className='service'>
+          <h2>Desarrollo WEB</h2>
+          <p>Creamos el software para adaptarlo a tus requerimientos</p>
+        </article>
+        <article className='service'>
+          <h2>Posicionamiento WEB</h2>
+          <p>Hacemos que tu web aparezca en los buscadores como se merece.</p>
+        </article>
+      </section>
     </div>
   )
 }
